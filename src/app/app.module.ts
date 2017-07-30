6df5f2e5cb0cd8effc5ef24e91c0a44d55104602
Jeferson Guardezi <jeferson@guardezi.com>
2017-07-30 15:14:26 -0300

    criado login e checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { CheckoutPage } from '../pages/checkout/checkout';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage,
+  LoginPage,
+  CheckoutPage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  BrowserModule,
+  IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage,
+  LoginPage,
+  CheckoutPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  StatusBar,
+  SplashScreen,
+  {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams, Slides,ModalController,LoadingController,Events} from 'ionic-angular';
+
+import { LoginPage } from '../login/login';
+import { CheckoutPage } from '../checkout/checkout';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +14,11 @@ export class HomePage {
 
   }
 
+  selectGift(){
+    this.navCtrl.push(LoginPage);
+  }
+
+  openCheckout(){
+    this.navCtrl.push(CheckoutPage);
+  }
 }
