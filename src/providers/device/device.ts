bd1ab48a88d57130eb1c7f5ac2be2aa9300004a3
Jeferson Guardezi <jeferson@guardezi.com>
2017-08-03 13:03:07 -0300

    ajustes

--- src/providers/device/device.ts
+++ src/providers/device/device.ts
@@ -33,8 +33,8 @@ export class DeviceProvider {
   put(url,object){
 
   }
-  API:string='http://localhost:5002/chadepanela-7e6d3/us-central1/';
-  // API:string='https://us-central1-chadepanela-7e6d3.cloudfunctions.net/';
+  // API:string='http://localhost:5002/chadepanela-7e6d3/us-central1/';
+  API:string='https://us-central1-chadepanela-7e6d3.cloudfunctions.net/';
   COUPLE:string='couple/9b6d4054-7c48-4b0e-9b00-4d5dc759a283';
   PAY:string='pay';
   // API:string='../assets/';
