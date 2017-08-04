d69f75231a552a5d5e85d38f66200efc9eceefab
Jeferson Guardezi <jeferson@guardezi.com>
2017-08-04 14:28:58 -0300

    ajuste nas fotos e tudo mais

--- src/providers/device/device.ts
+++ src/providers/device/device.ts
@@ -34,8 +34,8 @@ export class DeviceProvider {
 
   }
 
-  API:string='http://localhost:5000/chadepanela-7e6d3/us-central1/';
-  // API:string='https://us-central1-chadepanela-7e6d3.cloudfunctions.net/';
+  // API:string='http://localhost:5000/chadepanela-7e6d3/us-central1/';
+  API:string='https://us-central1-chadepanela-7e6d3.cloudfunctions.net/';
   COUPLE_ID:string='9b6d4054-7c48-4b0e-9b00-4d5dc759a283';
   COUPLE:string='couple/'+this.COUPLE_ID;
   PAY:string='pay';
